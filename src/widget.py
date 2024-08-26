83562a84a70d7f33aaa967403692ff89841ac47f
Ilya Gaifullin <proskmax@yandex.ru>
2024-08-27 04:17:20 +0500

    update src/widget.py

--- src/widget.py
+++ src/widget.py
@@ -1,12 +1,27 @@
-def mask_account_card(account_card):
+from typing import Union
+
+
+def mask_account_card(account_card: Union[str]) -> str:
     """Фуникция, которая умеет обрабатывать информацию как о картах, так и о счетах."""
 
     if account_card[0:4] == "Счет":
         return "Cчет" + " " + "**" + account_card[21:]
     else:
-        return account_card[:-16] + " " + account_card[-16:-12] + " " + account_card[-12:-10] + "**" + " " + "****" + " " + account_card[-4:]
+        return (
+            account_card[:-16]
+            + " "
+            + account_card[-16:-12]
+            + " "
+            + account_card[-12:-10]
+            + "**"
+            + " "
+            + "****"
+            + " "
+            + account_card[-4:]
+        )
 
-def get_date(date):
-    """Функция, которая принимает на вход строку с датой в формате "2024-03-11T02:26:18.671407" и возвращает строку с датой в формате "ДД.ММ.ГГГГ" """
 
+def get_date(date: Union[str]) -> str:
+    """Функция, которая принимает на вход строку с датой в формате "2024-03-11T02:26:18.671407"
+    и возвращает строку с датой в формате "ДД.ММ.ГГГГ" """
     return date[8:10] + "." + date[5:7] + "." + date[0:4]
